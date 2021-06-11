a4110cc20afdd3f52181da607648d7919b6c4947
7box7 <60572459+7box7@users.noreply.github.com>
2021-06-11 08:26:07 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,16 +1,9 @@
+from django.shortcuts import render, redirect
 from datetime import datetime
 
 from django.http import HttpResponse
 
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    context = {}
+    return render(request, "elevan_class/gen_info.html", context)
